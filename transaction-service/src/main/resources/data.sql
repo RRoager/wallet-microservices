897c9b44ecac0929b5056d601c65a5929d3c7e5b
RRoager <rasmus-roager@hotmail.com>
2023-01-23 13:59:36 +0100

    Updated data.sql with new data

--- transaction-service/src/main/resources/data.sql
+++ transaction-service/src/main/resources/data.sql
@@ -1,12 +1,12 @@
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (1, 1500.0, '2023-01-01', 'DEPOSIT');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (1, 500.0, '2023-01-03', 'WITHDRAW');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (1, 100.0, '2023-01-08', 'WITHDRAW');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (2, 300.0, '2023-01-02', 'DEPOSIT');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (2, 250.0, '2023-01-05', 'WITHDRAW');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (2, 800.0, '2023-01-06', 'DEPOSIT');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (3, 600.0, '2023-01-03', 'WITHDRAW');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (3, 5000.0, '2023-01-05', 'DEPOSIT');
-insert into TRANSACTIONS (wallet_id, amount, transaction_date, transaction_type) values (3, 2500.0, '2023-01-10', 'DEPOSIT');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (1, 1500.0, 5600.0, '2023-01-01', 'DEPOSIT');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (1, 500.0, 5100.0, '2023-01-03', 'WITHDRAW');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (1, 100.0, 5000.0, '2023-01-08', 'WITHDRAW');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (2, 300.0, 14450.0, '2023-01-02', 'DEPOSIT');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (2, 250.0, 14200.0, '2023-01-05', 'WITHDRAW');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (2, 800.0, 15000.0, '2023-01-06', 'DEPOSIT');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (3, 600.0, 17500.0, '2023-01-03', 'WITHDRAW');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (3, 5000.0, 22500.0, '2023-01-05', 'DEPOSIT');
+insert into TRANSACTIONS (wallet_id, amount, current_balance, transaction_date, transaction_type) values (3, 2500.0, 25000.0, '2023-01-10', 'DEPOSIT');
 
 
 
--- wallet-service/src/main/resources/data.sql
+++ wallet-service/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into WALLETS (balance) values (5000.0);
-insert into WALLETS (balance) values (15000.0);
-insert into WALLETS (balance) values (2500.0);
+insert into WALLETS (user_id, balance) values (1, 5000.0);
+insert into WALLETS (user_id, balance) values (2, 15000.0);
+insert into WALLETS (user_id, balance) values (3, 25000.0);
 
