84051256f73ffda3671f9eb8a8fa396690c49f3f
RRoager <rasmus-roager@hotmail.com>
2023-02-03 12:34:31 +0100

    Added @EqualsAndHashCode

--- transaction-service/src/main/java/com/rroager/transactionservice/entity/Transaction.java
+++ transaction-service/src/main/java/com/rroager/transactionservice/entity/Transaction.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -18,6 +19,7 @@ import java.sql.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 @Entity
 @Table(name = "transactions")
 public class Transaction {
